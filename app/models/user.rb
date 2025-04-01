9f060a14809fc607313ba8898ede01dd0acb01b5
Vishnu Atrai <vishnu.atrai+1@gmail.com>
2025-04-01 20:46:17 +0530

    generate devise user

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
+
+  validates :email, uniqueness: { case_sensitive: false }, presence: true  
+end
