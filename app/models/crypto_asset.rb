e2b9d27d6f7ce36473b86622e2a7a29541d90d23
Vishnu Atrai <vishnu.atrai+1@gmail.com>
2025-04-01 21:16:32 +0530

    model spec for crypto assets

--- app/models/crypto_asset.rb
+++ app/models/crypto_asset.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 class CryptoAsset < ApplicationRecord
   belongs_to :user
+
+  validates :user, :name, :symbol, :quantity, :purchase_price, presence: true
+  validates :name, :symbol, uniqueness: { case_sensitive: false }
+  validates :quantity, :purchase_price, :current_price, numericality: true
 end
--- spec/models/crypto_asset_spec.rb
+++ spec/models/crypto_asset_spec.rb
@@ -1,5 +1,21 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe CryptoAsset, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:user) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:symbol) }
+  it { should validate_presence_of(:quantity) }
+  it { should validate_presence_of(:purchase_price) }
+  it { should validate_numericality_of(:quantity) }
+  it { should validate_numericality_of(:purchase_price) }
+  it { should validate_numericality_of(:current_price) }
+
+  context 'when validate uniqueness' do
+    before { create(:crypto_asset) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
+    it { should validate_uniqueness_of(:symbol).case_insensitive }
+  end
 end
