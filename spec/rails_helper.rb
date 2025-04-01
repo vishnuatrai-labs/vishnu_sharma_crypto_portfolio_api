9c0fcc1e4214dadbe351b7591aae9e658577b203
Vishnu Atrai <vishnu.atrai+1@gmail.com>
2025-04-01 20:33:32 +0530

    shoulda include

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,14 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
@@ -67,4 +75,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryBot::Syntax::Methods
+  config.include Shoulda::Matchers::ActiveRecord
+  config.include Shoulda::Matchers::ActiveModel
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
