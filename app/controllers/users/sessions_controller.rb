c157d2ea289ca3903f760b3e243a284a1c243062
Vishnu Atrai <vishnu.atrai+1@gmail.com>
2025-04-01 21:32:47 +0530

    devise user session with jwt token

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,4 +24,20 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    render json: { token: current_token, user: resource }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    head :no_content
+  end
+
+  def current_token
+    request.env['warden-jwt_auth.token']
+  end
 end
